2617576f63da9579dca35dec401f4450fd37602a
Madhanprakash <30310262+Madhanprakash33@users.noreply.github.com>
2017-07-22 10:38:35 +0530

    Create simple calculator.c

---
+++ simple calculator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,t,s;
+    scanf("%d%d",&n,&t);
+    s=n+t;
+    printf("%d\n",s);
+    s=n-t;
+    printf("%d\n",s);
+    s=n/t;
+    printf("%d\n",s);
+    s=n*t;
+    printf("%d\n",s);
+    return 0;
+}
+
